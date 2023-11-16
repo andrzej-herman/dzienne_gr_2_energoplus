1187c992b5670f7c7e42d3f332f050a676d5d051
Andrzej Herman <andrzejek.herman@gmail.com>
2023-11-16 11:02:18 +0100

    Add project

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+      <div className="h-full flex items-center justify-center">
+          <p className="text-7xl font-bold tracking-tight text-blue-700">Społeczna Akademia Nauk</p>
+      </div>
+  )
+}
